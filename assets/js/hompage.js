3b20a2ddc0520a16e2a1408934b307f415de346a
David Daly <dalyd14@gmail.com>
2020-11-16 15:05:34 -0500

    added code to display repos based on the language

--- assets/js/hompage.js
+++ assets/js/hompage.js
@@ -4,6 +4,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons")
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -86,4 +88,28 @@ var formSubmitHandler = function(event) {
     }
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Errors: " + response.statusText)
+        }
+    });
+}
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    if (language) {
+        getFeaturedRepos(language)
+
+        repoContainerEl.textContent = ""
+    }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler)
 userFormEl.addEventListener("submit", formSubmitHandler);
